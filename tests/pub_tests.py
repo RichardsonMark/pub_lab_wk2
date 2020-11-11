1d1ca5bfc57ffca1adade219b35c2bc884770e8c
Mark Richardson <mark_richardson@sky.com>
2020-11-11 12:07:07 +0000

    Adding Pub class and ran first 2 tests

---
+++ tests/pub_tests.py
@@ -0,0 +1,25 @@
+import unittest
+from src.pub import Pub
+
+
+class TestPub(unittest.TestCase):
+    def setUp(self):
+        self.pub = Pub("One way Tolbooth", 500)
+
+        self.drink_1 =  Drink("Super T", 3)
+        self.drink_2 =  Drink("Buckfast", 5)
+        self.drink_3 =  Drink("Mysterious Cocktail", 4)
+        self.drink_4 =  Drink("House wine", 7)
+        
+
+        drinks = [self.drink_1, self.drink_2, self.drink_3, self.drink_4,]
+         
+    
+    @unittest.skip("Delete this line to run the test")
+    def test_has_pub_name(self):
+        self.assertEqual("One way Tolbooth", self.pub.name)
+
+    @unittest.skip("Delete this line to run the test")
+    def test_has_till(self):
+        self.assertEqual(500, self.pub.till)
+
